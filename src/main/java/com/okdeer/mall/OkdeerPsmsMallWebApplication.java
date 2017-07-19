d6fc2eab61c392104443a57d9d0a0c176433907e
xuzq01 <xuzq01@okdeer.com>
2017-07-19 18:16:33 +0800

    app提交

---
+++ src/main/java/com/okdeer/mall/OkdeerPsmsMallWebApplication.java
@@ -0,0 +1,12 @@
+package com.okdeer.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkdeerPsmsMallWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkdeerPsmsMallWebApplication.class, args);
+	}
+}
